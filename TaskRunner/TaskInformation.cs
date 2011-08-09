f1a67613d057ced337ab71a1138180992c7fc5f0
Kristof Claes <kristof.c@gmail.com>
2011-08-09 20:03:40 +0200

    ToString override in TaskInformation

--- TaskRunner/TaskInformation.cs
+++ TaskRunner/TaskInformation.cs
@@ -28,7 +28,17 @@ namespace TaskRunner
         private void GetDescription()
         {
             var attributes = TaskType.GetCustomAttributes(true).Where(a => a is TaskDescriptionAttribute).FirstOrDefault() as TaskDescriptionAttribute;
-            Description = attributes == null ? "" : attributes.Description;
+            Description = attributes == null ? String.Empty : attributes.Description;
+        }
+
+        public override string ToString()
+        {
+            if(HasDescription)
+            {
+                return String.Format("{0} ({1})", Name, Description);
+            }
+            
+            return Name;
         }
     }
 }
